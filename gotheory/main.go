1e9a8b13c56c7404f3280cbad7a64d69025d9798
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-01 01:35:37 +0530

    first commit

--- gotheory/main.go
+++ gotheory/main.go
@@ -1,3 +1,21 @@
-func main() {
+// package main
 
-}
+// import (
+// 	"fmt"
+// )
+
+// type User struct {
+// 	username string
+// 	password string
+// }
+
+// func (auth User) getAuth() string {
+// 	return fmt.Sprintf("authorization:basix %s:%s", auth.username, auth.password)
+// }
+// func main() {
+
+// 	fmt.Println()
+// }
+
+//interfaces
+
